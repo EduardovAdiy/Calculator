41d7f602b4f8bfcbc741a68da961a9b9d015ada7
Адий Эдуардов <oshakbaev_adi@mail.ru>
2025-10-18 16:30:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,69 @@
 'use strict';
 
 let result = '';
+let curNumber = '';
 let lastAction ='';
 
 function checkElementId (event){
     let button = event.target.id;
+    let data = '';
 
     if(!isNaN(Number(button))) {
-        if (button > 0) {
-            result += String(button);
-            changeOutputText(result); 
+       inputNumber(button);
+    } else {
+        switch(button){
+            case '%':
+                return 'findPercent';
+                break;
+            case 'CE':
+                result = '';
+                curNumber = '';
+                changeOutputText ('0');
+                break;
+            case 'C':
+                curNumber = '';
+                changeOutputText ('0');
+                break;
+            case "DEL":
+                 data = getOutputText();
+                changeOutputText (data.substring(0, (data.length - 1)));
+                break;
+            case '1/x':
+                data = getOutputText();
+                break;
+            case 'x^2':
+                return 'sqr'
+                break;
+            case 'SQRT(x)':
+                return 'sqrt'
+                break;
+            case '÷':
+                return 'devide'
+                break;
         }
     }
 }
 
 function changeOutputText (data) {
     let div = document.getElementsByClassName("output-text")[0];
-    console.log(data, div)
-    div.innerHTML = data;
-    
+    div.innerHTML = data;  
+}
+
+function getOutputText () {
+    let div = document.getElementsByClassName("output-text")[0];
+
+    return div.innerHTML;
+}
+
+function inputNumber(data) {
+    let outputText = getOutputText();
+
+    if (outputText == 0 && data == 0) {
+        changeOutputText ('0');
+    } else {
+        curNumber += String(data);
+        changeOutputText (curNumber);
+    }
 }
 
 function generateButtons() {
